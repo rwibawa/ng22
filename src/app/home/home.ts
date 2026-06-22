d482865da667caedb75ff318ff311b6cef7765db
Ryan Wibawa <ryan.wibawa@gmail.com>
2026-06-22 08:20:07 +0000

    add housingLocation

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationInfo } from '../housing-location';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   template: `
    <section>
     <form>
       <input type="text" placeholder="Filter by city" />
       <button class="primary" type="button">Search</button>
     </form>
+  </section>
+  <section class="results">
+    <app-housing-location [housingLocation]="housingLocation" />
   </section> 
    `,
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
