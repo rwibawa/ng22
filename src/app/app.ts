b3270cc3ab71562996d31315283177f8f8006443
Ryan Wibawa <ryan.wibawa@gmail.com>
2026-06-22 06:01:23 +0000

    add home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [Home], // [RouterOutlet],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
+  // templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ng22');
+  title = 'default';
+  // protected readonly title = signal('ng22');
 }
---
+++ src/app/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: ` <p>home works!</p> `,
+  styleUrl: './home.css',
+})
+export class Home {}
